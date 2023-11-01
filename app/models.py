a6874e9ce5230e329296ce82e5ae1ea7cb40d87d
Kai W. Li <stepperpig@gmail.com>
2023-10-31 22:33:03 -0700

    added followers association table

--- app/models.py
+++ app/models.py
@@ -30,6 +30,11 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
