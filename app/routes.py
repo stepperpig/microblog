aeafa2950e98b124dca6c88cda295d8d3a05fe6d
Kai W. Li <stepperpig@gmail.com>
2023-11-06 16:53:44 -0800

    cont from prev commit

--- app/routes.py
+++ app/routes.py
@@ -131,5 +131,6 @@ def unfollow(username):
 @app.route('/explore')
 @login_required
 def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', title='Explore', posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    return render_template('index.html', title='Explore', posts=posts.items)
