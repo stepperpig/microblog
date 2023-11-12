24bde55f7854022703923664279e41530e4eb169
Kai W. Li <stepperpig@gmail.com>
2023-11-11 21:31:54 -0800

    bootstrap redesign

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,6 +13,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
